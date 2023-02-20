bceb6555491b5eccff76aaa863a1d01871392047
Phoebe C <63136975+pc027@users.noreply.github.com>
2023-02-20 19:42:59 +0000

    initial thoughts about dfa

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,23 @@
 package dfa // leave this line in the file
 
 // TODO: replace this comment with your implementation
+case class State(label: String)
+case class Transition(from: State, symbol: Char, to: State)
+
+class DFA(val state: Set[State], val transitions: Set[Transition],
+    val startState: State, val acceptingStates: Set[State]):
+
+    def accept(line: String): Boolean = {
+        var currState = startState
+        // char by char -> for loop c
+        for (c <- line) {
+            // TODO: find transition(from: currState, symbol: currChar)
+            val currTransition = transitions.head
+
+            // set new state
+            currState = currTransition.to
+        }
+
+        if acceptingStates.contains(currState) then true
+        else false
+    }
